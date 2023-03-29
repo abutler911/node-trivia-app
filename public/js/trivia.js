b31a40808312565dff25a3bed095b0b65ac1c970
abutler911 <abutler911@gmail.com>
2023-03-28 21:16:53 -0600

    cleaned up some code

--- public/js/trivia.js
+++ public/js/trivia.js
@@ -1,31 +1,23 @@
-const player1Name = "<%= player1Name %>";
-const player2Name = "<%= player2Name %>";
-let currentQuestionIndex = 0;
-const questions = JSON.parse(document.getElementById("questions").value);
-
-let player1Score = parseInt(localStorage.getItem("player1Score")) || 0;
-let player2Score = parseInt(localStorage.getItem("player2Score")) || 0;
-
+// Variables
+const player1NameEl = document.getElementById("player1Name");
+const player2NameEl = document.getElementById("player2Name");
 const player1ScoreEl = document.getElementById("player1Score");
 const player2ScoreEl = document.getElementById("player2Score");
+const questions = JSON.parse(document.getElementById("questions").value);
+let player1Score = parseInt(localStorage.getItem("player1Score")) || 0;
+let player2Score = parseInt(localStorage.getItem("player2Score")) || 0;
+let currentQuestionIndex = 0;
 
+// Initial score display
 player1ScoreEl.textContent = player1Score;
 player2ScoreEl.textContent = player2Score;
 
+// Event Listeners
 document.getElementById("player1Increment").addEventListener("click", () => {
   player1Score++;
   player1ScoreEl.textContent = player1Score;
   localStorage.setItem("player1Score", player1Score);
-  if (player1Score > player2Score) {
-    document.getElementById("player1Name").style.color = "green";
-    document.getElementById("player2Name").style.color = "red";
-  } else if (player1Score < player2Score) {
-    document.getElementById("player1Name").style.color = "red";
-    document.getElementById("player2Name").style.color = "green";
-  } else {
-    document.getElementById("player1Name").style.color = "";
-    document.getElementById("player2Name").style.color = "";
-  }
+  updatePlayerColors();
 });
 
 document.getElementById("player1Decrement").addEventListener("click", () => {
@@ -33,16 +25,7 @@ document.getElementById("player1Decrement").addEventListener("click", () => {
     player1Score--;
     player1ScoreEl.textContent = player1Score;
     localStorage.setItem("player1Score", player1Score);
-  }
-  if (player1Score > player2Score) {
-    document.getElementById("player1Name").style.color = "green";
-    document.getElementById("player2Name").style.color = "red";
-  } else if (player1Score < player2Score) {
-    document.getElementById("player1Name").style.color = "red";
-    document.getElementById("player2Name").style.color = "green";
-  } else {
-    document.getElementById("player1Name").style.color = "";
-    document.getElementById("player2Name").style.color = "";
+    updatePlayerColors();
   }
 });
 
@@ -50,16 +33,7 @@ document.getElementById("player2Increment").addEventListener("click", () => {
   player2Score++;
   player2ScoreEl.textContent = player2Score;
   localStorage.setItem("player2Score", player2Score);
-  if (player1Score > player2Score) {
-    document.getElementById("player1Name").style.color = "green";
-    document.getElementById("player2Name").style.color = "red";
-  } else if (player1Score < player2Score) {
-    document.getElementById("player1Name").style.color = "red";
-    document.getElementById("player2Name").style.color = "green";
-  } else {
-    document.getElementById("player1Name").style.color = "";
-    document.getElementById("player2Name").style.color = "";
-  }
+  updatePlayerColors();
 });
 
 document.getElementById("player2Decrement").addEventListener("click", () => {
@@ -67,22 +41,13 @@ document.getElementById("player2Decrement").addEventListener("click", () => {
     player2Score--;
     player2ScoreEl.textContent = player2Score;
     localStorage.setItem("player2Score", player2Score);
-  }
-  if (player1Score > player2Score) {
-    document.getElementById("player1Name").style.color = "green";
-    document.getElementById("player2Name").style.color = "red";
-  } else if (player1Score < player2Score) {
-    document.getElementById("player1Name").style.color = "red";
-    document.getElementById("player2Name").style.color = "green";
-  } else {
-    document.getElementById("player1Name").style.color = "";
-    document.getElementById("player2Name").style.color = "";
+    updatePlayerColors();
   }
 });
 
 document.getElementById("resetButton").addEventListener("click", resetGame);
 
-document.getElementById("show-answer").addEventListener("click", function () {
+document.getElementById("show-answer").addEventListener("click", () => {
   const listItems = document.querySelectorAll("li");
   listItems.forEach((item) => {
     if (item.getAttribute("data-correct") === "true") {
@@ -92,48 +57,13 @@ document.getElementById("show-answer").addEventListener("click", function () {
   });
 });
 
-function resetGame() {
-  localStorage.setItem("player1Score", 0);
-  localStorage.setItem("player2Score", 0);
-  window.location.href = "/";
-}
-
-function loadNextQuestion() {
-  if (currentQuestionIndex < questions.length - 1) {
-    currentQuestionIndex++;
-    const nextQuestion = questions[currentQuestionIndex];
-    // Update the question text and answers
-    document.querySelector(".card-title").textContent = nextQuestion.question;
-    const listItems = document.querySelectorAll("li");
-    listItems.forEach((item, index) => {
-      item.textContent = nextQuestion.allAnswers[index];
-      item.style.color = "";
-      item.style.fontSize = "";
-      item.setAttribute(
-        "data-correct",
-        item.textContent === nextQuestion.correct_answer
-      );
-    });
-  } else {
-    alert("No more questions available.");
-  }
-}
-
 document.getElementById("nextButton").addEventListener("click", () => {
-  const questions = JSON.parse(document.getElementById("questions").value);
   currentQuestionIndex++;
-
   if (currentQuestionIndex < questions.length) {
     const nextQuestion = questions[currentQuestionIndex];
-    const answerList = document.getElementById("answerList");
-
-    // Update the question text
     document.getElementById("questionText").textContent = nextQuestion.question;
-
-    // Clear the current answers
+    const answerList = document.getElementById("answerList");
     answerList.innerHTML = "";
-
-    // Add the new answers
     nextQuestion.allAnswers.forEach((answer) => {
       const li = document.createElement("li");
       li.textContent = answer;
@@ -144,3 +74,23 @@ document.getElementById("nextButton").addEventListener("click", () => {
     alert("You have reached the end of the questions.");
   }
 });
+
+// Functions
+function updatePlayerColors() {
+  if (player1Score > player2Score) {
+    player1NameEl.style.color = "green";
+    player2NameEl.style.color = "red";
+  } else if (player1Score < player2Score) {
+    player1NameEl.style.color = "red";
+    player2NameEl.style.color = "green";
+  } else {
+    player1NameEl.style.color = "";
+    player2NameEl.style.color = "";
+  }
+}
+
+function resetGame() {
+  localStorage.setItem("player1Score", 0);
+  localStorage.setItem("player2Score", 0);
+  window.location.href = "/";
+}
